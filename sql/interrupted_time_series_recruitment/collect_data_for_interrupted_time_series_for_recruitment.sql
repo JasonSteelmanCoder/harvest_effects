bf361313e0ebe1ef2f78d3f8e1d33e0231dce27a
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-09-19 10:44:06 -0400

    collect data for interrupted time series for recruitment

--- sql/interrupted_time_series_recruitment/collect_data_for_interrupted_time_series_for_recruitment.sql
+++ sql/interrupted_time_series_recruitment/collect_data_for_interrupted_time_series_for_recruitment.sql
@@ -1,144 +1,98 @@
-WITH plot_years AS (
-	-- grab plot-years 
-		-- aggregate trtcds from all of the conditions (remember that there may be more than one condition per plot)
-		-- aggregate agg_trtcds1-3 into one yearly_trtcd
+WITH row_per_tree_associated AS (
+	WITH row_per_tree AS (
+		WITH plot_years AS (
+			-- grab plot-years 
+				-- aggregate trtcds from all of the conditions (remember that there may be more than one condition per plot)
+				-- aggregate agg_trtcds1-3 into one yearly_trtcd
+			SELECT 
+				euc.statecd,
+				euc.unitcd,
+				euc.countycd,
+				euc.plot,
+				euc.invyr,
+				MAX(CASE WHEN euc.trtcd1 = 10 THEN 10 ELSE 0 END) AS agg_trtcd1,
+				MAX(CASE WHEN euc.trtcd2 = 10 THEN 10 ELSE 0 END) AS agg_trtcd2,
+				MAX(CASE WHEN euc.trtcd3 = 10 THEN 10 ELSE 0 END) AS agg_trtcd3,
+				CASE 
+					WHEN (
+						MAX(CASE WHEN euc.trtcd1 = 10 THEN 10 ELSE 0 END) = 10
+						OR MAX(CASE WHEN euc.trtcd2 = 10 THEN 10 ELSE 0 END) = 10
+						OR MAX(CASE WHEN euc.trtcd3 = 10 THEN 10 ELSE 0 END) = 10
+					) 
+					THEN 10 
+					ELSE 0 
+				END AS yearly_trtcd
+			FROM east_us_cond euc
+			WHERE 
+				euc.trtcd1 IS NOT NULL
+				AND euc.trtcd2 IS NOT NULL
+				AND euc.trtcd3 IS NOT NULL
+			GROUP BY
+				euc.statecd,
+				euc.unitcd,
+				euc.countycd,
+				euc.plot,
+				euc.invyr
+			ORDER BY 
+				euc.statecd,
+				euc.unitcd,
+				euc.countycd,
+				euc.plot,
+				euc.invyr
+		)
+		-- make a row for each tree (include association)
+		SELECT 
+			py.statecd,
+			py.unitcd,
+			py.countycd,
+			py.plot,
+			py.invyr,
+			py.agg_trtcd1,
+			py.agg_trtcd2,
+			py.agg_trtcd3,
+			py.yearly_trtcd,
+			eut.spcd,
+			eut.statuscd
+		FROM plot_years py
+		LEFT JOIN east_us_tree eut		-- left join allows plot-years with zero trees
+		ON 
+			eut.statecd = py.statecd
+			AND eut.unitcd = py.unitcd
+			AND eut.countycd = py.countycd
+			AND eut.plot = py.plot
+			AND eut.invyr = py.invyr
+	)
+	-- make spcd into association
 	SELECT 
-		euc.statecd,
-		euc.unitcd,
-		euc.countycd,
-		euc.plot,
-		euc.invyr,
-		MAX(CASE WHEN euc.trtcd1 = 10 THEN 10 ELSE 0 END) AS agg_trtcd1,
-		MAX(CASE WHEN euc.trtcd2 = 10 THEN 10 ELSE 0 END) AS agg_trtcd2,
-		MAX(CASE WHEN euc.trtcd3 = 10 THEN 10 ELSE 0 END) AS agg_trtcd3,
-		CASE 
-			WHEN (
-				MAX(CASE WHEN euc.trtcd1 = 10 THEN 10 ELSE 0 END) = 10
-				OR MAX(CASE WHEN euc.trtcd2 = 10 THEN 10 ELSE 0 END) = 10
-				OR MAX(CASE WHEN euc.trtcd3 = 10 THEN 10 ELSE 0 END) = 10
-			) 
-			THEN 10 
-			ELSE 0 
-		END AS yearly_trtcd
-	FROM east_us_cond euc
-	WHERE 
-		euc.trtcd1 IS NOT NULL
-		AND euc.trtcd2 IS NOT NULL
-		AND euc.trtcd3 IS NOT NULL
-	GROUP BY
-		euc.statecd,
-		euc.unitcd,
-		euc.countycd,
-		euc.plot,
-		euc.invyr
-	ORDER BY 
-		euc.statecd,
-		euc.unitcd,
-		euc.countycd,
-		euc.plot,
-		euc.invyr
+		statecd,
+		unitcd,
+		countycd,
+		plot,
+		invyr,
+		yearly_trtcd,
+		rs.association,
+		statuscd
+	FROM row_per_tree rpt
+	LEFT JOIN ref_species rs 
+	ON rs.spcd = rpt.spcd
 )
--- grab all thrice-obserced plots with their list of obs_years and harvest codes
-SELECT 
-	py.statecd,
-	py.unitcd,
-	py.countycd,
-	py.plot,
-	ARRAY_AGG(py.invyr ORDER BY py.invyr) AS obs_years,
-	ARRAY_AGG(py.yearly_trtcd ORDER BY py.invyr) AS trtcds
-FROM plot_years py
+-- group by plot-year while counting AM and EM trees
+SELECT
+	statecd, 
+	unitcd,
+	countycd,
+	plot, 
+	invyr,
+	MAX(yearly_trtcd) AS yearly_trtcd,			-- there is only one yearly_trtcd per plot-year, so keep it the same.
+	COUNT(CASE WHEN association = 'AM' AND statuscd = 1 THEN 1 ELSE 0 END) AS am_trees,
+	COUNT(CASE WHEN association = 'EM' AND statuscd = 1 THEN 1 ELSE 0 END) AS em_trees,
+	COUNT(CASE WHEN (association != 'AM' AND association != 'EM' AND statuscd = 1) THEN 1 ELSE 0 END) AS other_trees
+FROM row_per_tree_associated rpta
 GROUP BY 
-	py.statecd,
-	py.unitcd,
-	py.countycd,
-	py.plot
-HAVING 
-	ARRAY_LENGTH(ARRAY_AGG(py.invyr ORDER BY py.invyr), 1) > 2		-- only accept plots that are observed at least three times
+	statecd, 
+	unitcd,
+	countycd,
+	plot, 
+	invyr
 
-
-
--- still to account for: counts of am and em trees
-
-
-
-
-
-
-
--- EXPERIMENTAL INNER QUERY
-WITH plot_years AS (
-	-- grab plot-years 
-		-- aggregate trtcds from all of the conditions (remember that there may be more than one condition per plot)
-		-- aggregate agg_trtcds1-3 into one yearly_trtcd
-	SELECT 
-		euc.statecd,
-		euc.unitcd,
-		euc.countycd,
-		euc.plot,
-		euc.invyr,
-		MAX(CASE WHEN euc.trtcd1 = 10 THEN 10 ELSE 0 END) AS agg_trtcd1,
-		MAX(CASE WHEN euc.trtcd2 = 10 THEN 10 ELSE 0 END) AS agg_trtcd2,
-		MAX(CASE WHEN euc.trtcd3 = 10 THEN 10 ELSE 0 END) AS agg_trtcd3,
-		CASE 
-			WHEN (
-				MAX(CASE WHEN euc.trtcd1 = 10 THEN 10 ELSE 0 END) = 10
-				OR MAX(CASE WHEN euc.trtcd2 = 10 THEN 10 ELSE 0 END) = 10
-				OR MAX(CASE WHEN euc.trtcd3 = 10 THEN 10 ELSE 0 END) = 10
-			) 
-			THEN 10 
-			ELSE 0 
-		END AS yearly_trtcd
-	FROM east_us_cond euc
-	WHERE 
-		euc.trtcd1 IS NOT NULL
-		AND euc.trtcd2 IS NOT NULL
-		AND euc.trtcd3 IS NOT NULL
-	GROUP BY
-		euc.statecd,
-		euc.unitcd,
-		euc.countycd,
-		euc.plot,
-		euc.invyr
-	ORDER BY 
-		euc.statecd,
-		euc.unitcd,
-		euc.countycd,
-		euc.plot,
-		euc.invyr
-)
--- add columns with AM and EM tree counts
-SELECT 
-	py.statecd,
-	py.unitcd,
-	py.countycd,
-	py.plot,
-	py.invyr,
-	py.agg_trtcd1,
-	py.agg_trtcd2,
-	py.agg_trtcd3,
-	py.yearly_trtcd,
-	SUM(CASE WHEN rs.association = 'AM' THEN 1 ELSE 0 END) AS am_trees,
-	SUM(CASE WHEN rs.association = 'EM' THEN 1 ELSE 0 END) AS em_trees,
-	SUM(CASE WHEN rs.association != 'AM' AND rs.association != 'EM' THEN 1 ELSE 0 END) AS other_trees
-FROM plot_years py
-JOIN east_us_tree eut
-ON 
-	eut.statecd = py.statecd
-	AND eut.unitcd = py.unitcd
-	AND eut.countycd = py.countycd
-	AND eut.plot = py.plot
-	AND eut.invyr = py.invyr
-JOIN ref_species rs
-ON eut.spcd = rs.spcd
-WHERE eut.statuscd = 1 		-- only count live trees
-GROUP BY 
-	py.statecd,
-	py.unitcd,
-	py.countycd,
-	py.plot,
-	py.invyr,
-	py.agg_trtcd1,
-	py.agg_trtcd2,
-	py.agg_trtcd3,
-	py.yearly_trtcd
-
+-- still to account for: test counts on individual plots
