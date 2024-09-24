33479d3ac70639aa37b52bebed440e8793f8efad
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-09-24 14:37:15 -0400

    update individual tree finder to work across all 26 states

--- sql/find_individual_trees_with_all_observations.sql
+++ sql/find_individual_trees_with_all_observations.sql
@@ -1,37 +1,37 @@
 WITH RECURSIVE tree_cte AS (
 	
 	SELECT 
-		tre_cn AS root_cn,
-		array[tre_cn]::varchar[] AS cn_sequence
-	FROM tree_grm_component
+		tre_cn AS original_cn,
+		array[tre_cn]::bigint[] AS cn_sequence
+	FROM east_us_tree_grm_component
 	WHERE prev_tre_cn IS NULL
 	
 	UNION ALL
 
 	SELECT 
-		tree_cte.root_cn,
-		tree_cte.cn_sequence || tree_grm_component.tre_cn
+		tree_cte.original_cn,
+		tree_cte.cn_sequence || east_us_tree_grm_component.tre_cn
 	FROM tree_cte
-	JOIN tree_grm_component
-	ON tree_grm_component.prev_tre_cn = tree_cte.cn_sequence[ARRAY_LENGTH(tree_cte.cn_sequence, 1)]
+	JOIN east_us_tree_grm_component
+	ON east_us_tree_grm_component.prev_tre_cn = tree_cte.cn_sequence[ARRAY_LENGTH(tree_cte.cn_sequence, 1)]
 		
 ), obs_nums AS (
 	SELECT 
-		root_cn, 
+		original_cn, 
 		MAX(ARRAY_LENGTH(cn_sequence, 1))  AS num
 	FROM tree_cte
-	GROUP BY root_cn
+	GROUP BY original_cn
 )
 SELECT 
-	tree_cte.root_cn, 
+	tree_cte.original_cn, 
 	tree_cte.cn_sequence
 FROM tree_cte
 JOIN obs_nums
 ON 
-	tree_cte.root_cn = obs_nums.root_cn
+	tree_cte.original_cn = obs_nums.original_cn
 	AND ARRAY_LENGTH(tree_cte.cn_sequence, 1) = obs_nums.num
-	WHERE ARRAY_LENGTH(tree_cte.cn_sequence, 1) > 1
-ORDER BY ARRAY_LENGTH(tree_cte.cn_sequence, 1) DESC
+	WHERE num > 1
+ORDER BY num DESC
 
 
 
