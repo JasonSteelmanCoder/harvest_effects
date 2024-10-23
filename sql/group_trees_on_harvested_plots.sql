f7519b30197bda869379350055349e08038daadf
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-10-23 14:55:55 -0400

    start grouping into plots and aggregating tree counts

--- sql/group_trees_on_harvested_plots.sql
+++ sql/group_trees_on_harvested_plots.sql
@@ -1,21 +1,95 @@
--- group into plot observations, aggregating trees into counts
-SELECT 
-	SUM(CASE WHEN statuscd != 3 AND statuscd IS NOT NULL THEN 1 ELSE 0 END) AS total_trees, 
-	SUM(CASE WHEN association = 'AM' AND statuscd != 3 AND statuscd IS NOT NULL THEN 1 ELSE 0 END) AS am_trees,
-	SUM(CASE WHEN association = 'EM' AND statuscd != 3 AND statuscd IS NOT NULL THEN 1 ELSE 0 END) AS em_trees,
-	SUM(CASE WHEN death_year = obs_year THEN 1 ELSE 0 END) AS total_dying_trees,
-	SUM(CASE WHEN death_year = obs_year AND association = 'AM' THEN 1 ELSE 0 END) AS dying_am_trees,
-	SUM(CASE WHEN death_year = obs_year AND association = 'EM' THEN 1 ELSE 0 END) AS dying_em_trees,
-	current_plot_cn, 
-	original_plot_cn,
-	first_harvest_year,
-	obs_year,
-	obs_number
-FROM a_temp
-GROUP BY 
-	current_plot_cn, 
-	original_plot_cn,
-	first_harvest_year,
-	obs_year,
-	obs_number
+WITH t1_tree_observations AS (
+	-- grab all of the tree observations that happened after a plot's first observation, but before the plot's first harvest
+	-- number the observations of individual trees (to help count distinct trees instead of observations)
+	SELECT 
+		ROW_NUMBER() OVER (
+			PARTITION BY original_tree_cn
+			ORDER BY current_year
+		) AS during_t1_tree_obs_number,
+		original_tree_cn,
+		association,
+		statuscd,
+		death_year,
+		obs_number,
+		current_year,
+		first_plot_obs_year,
+		last_pre_harvest_year,
+		first_harvest_year,
+		last_plot_obs_year, 
+		original_plot_cn,
+		current_plot_cn
+	FROM a_temp 
+	WHERE 
+		current_year > first_plot_obs_year		-- t1 trees are observed *after* the first plot observation
+		AND current_year < first_harvest_year		-- t1 trees are observed before the first harvest observation
+),
 
+t1_plots AS (
+	SELECT 
+		COUNT(original_tree_cn) AS t1_population, 
+		first_plot_obs_year,
+		last_pre_harvest_year, 
+		first_harvest_year,
+		last_plot_obs_year,
+		original_plot_cn
+	FROM t1_tree_observations
+	WHERE during_t1_tree_obs_number = 1		-- prevent duplicates for trees observed twice before harvesting
+	GROUP BY 
+		first_plot_obs_year,
+		last_pre_harvest_year,
+		first_harvest_year,
+		last_plot_obs_year,
+		original_plot_cn
+	ORDER BY 
+		original_plot_cn 
+),
+
+t2_tree_observations AS (
+	-- grab all of the tree observations that happened after a plot's first harvest observation
+	-- number the observations of individual trees (to help count distinct trees instead of observations)
+	SELECT 
+		ROW_NUMBER() OVER (
+			PARTITION BY original_tree_cn
+			ORDER BY current_year
+		) AS during_t2_tree_obs_number,
+		original_tree_cn,
+		association,
+		statuscd,
+		death_year,
+		obs_number,
+		current_year,
+		first_plot_obs_year,
+		last_pre_harvest_year,
+		first_harvest_year,
+		last_plot_obs_year, 
+		original_plot_cn,
+		current_plot_cn
+	FROM a_temp 
+	WHERE 
+		current_year > first_harvest_year		-- t2 trees are observed *after* the first harvest observation
+),
+
+t2_plots AS (
+	SELECT 
+		COUNT(original_tree_cn) AS t2_population, 
+		first_plot_obs_year,
+		last_pre_harvest_year, 
+		first_harvest_year,
+		last_plot_obs_year,
+		original_plot_cn
+	FROM t2_tree_observations
+	WHERE during_t2_tree_obs_number = 1		-- prevent duplicates for trees observed twice before harvesting
+	GROUP BY 
+		first_plot_obs_year,
+		last_pre_harvest_year,
+		first_harvest_year,
+		last_plot_obs_year,
+		original_plot_cn
+	ORDER BY 
+		original_plot_cn 	
+)
+--
+SELECT * 
+FROM t2_plots
+
+	
