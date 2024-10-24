2b7d144e5f04ec366da9aae140ce0c640f91fe0f
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-10-24 14:37:21 -0400

    fix mathematical typo

--- python/calculate_change_in_deaths_per_capita_for_am_vs_em.py
+++ python/calculate_change_in_deaths_per_capita_for_am_vs_em.py
@@ -126,7 +126,7 @@ for val in delta_em_deaths_list:
     if val is not None:
         all_unfiltered_means.append(val)
 pooled_std_dev = np.std(all_unfiltered_means)
-cohens_d = mean_am_delta_deaths_before_filtering - mean_em_delta_deaths_before_filtering / pooled_std_dev
+cohens_d = (mean_am_delta_deaths_before_filtering - mean_em_delta_deaths_before_filtering) / pooled_std_dev
 
 print(f"effect size: {cohens_d}")
 print()
