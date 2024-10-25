171847e8523d47d9b0538f5739e5bc4d014db42f
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-10-25 08:08:54 -0400

    calculate mean trees and mean deaths per plot

--- python/find_metastats_on_mortality_analysis.py
+++ python/find_metastats_on_mortality_analysis.py
@@ -15,6 +15,10 @@ load_dotenv()
 t1_timespans = []
 t2_timespans = []
 total_timespans = []
+t1_tree_counts = []
+t2_tree_counts = []
+t1_death_counts = []
+t2_death_counts = []
 
 # go through each row of the csv
 with open(f'C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/harvest_data/mortality/plots_with_tree_counts_and_death_counts_for_t1_and_t2.csv') as input:
@@ -47,15 +51,36 @@ with open(f'C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/harvest_data/mortality/
         t2_timespans.append(t2_timespan)
         total_timespans.append(last_plot_obs_year - first_plot_obs_year)
 
+        t1_tree_counts.append(t1_am_tree_count + t1_em_tree_count)
+        t2_tree_counts.append(t2_am_tree_count + t2_em_tree_count)
+
+        t1_death_counts.append(t1_am_death_count + t1_em_death_count)
+        t2_death_counts.append(t2_am_death_count + t2_em_death_count)
 
 # print stats
 mean_t1_timespan = np.mean(t1_timespans)
 mean_t2_timespan = np.mean(t2_timespans)
 mean_total_timespan = np.mean(total_timespans)
 
+print()
 print(f"mean t1 timespan: {mean_t1_timespan}")
 print(f"mean t2 timespan: {mean_t2_timespan}")
 print(f"mean total timespan: {mean_total_timespan}")
+print()
+
+mean_t1_tree_count = np.mean(t1_tree_counts)
+mean_t2_tree_count = np.mean(t2_tree_counts)
+
+print(f"mean number of trees in a plot during t1: {mean_t1_tree_count}")
+print(f"mean number of trees in a plot during t2: {mean_t2_tree_count}")
+print()
+
+mean_t1_death_count = np.mean(t1_death_counts)
+mean_t2_death_count = np.mean(t2_death_counts)
+
+print(f"mean number of deaths in a plot during t1: {mean_t1_death_count}")
+print(f"mean number of deaths in a plot during t2: {mean_t2_death_count}")
+print()
 
 # plot distributions
 fig, (ax1, ax2) = plt.subplots(1, 2)
