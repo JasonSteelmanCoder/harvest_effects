fix calculation of effect size

python/growth_time_series_analysis.py
@@ -75,8 +75,7 @@ print(f"p-value: {pvalue}")
 print()
 
 # find the effect size
-all_rate_changes = am_rate_changes + em_rate_changes
-pooled_std = np.std(all_rate_changes)
+pooled_std = np.sqrt((np.std(am_rate_changes) ** 2 + np.std(em_rate_changes) ** 2) / 2)
 cohens_d = (am_mean_change - em_mean_change) / pooled_std
 print(f"effect size: {cohens_d}")
 print()

python/tree_count_time_series_analysis.py
@@ -137,8 +137,7 @@ print(f"p-value: {p_value}")
 print()
 
 # calculate effect size
-all_changes_in_slope = changes_in_am_slope + changes_in_em_slope
-pooled_std_dev = np.std(all_changes_in_slope)
+pooled_std_dev = np.sqrt((np.std(changes_in_am_slope) ** 2 + np.std(changes_in_em_slope) ** 2) / 2)
 cohens_d = (mean(changes_in_am_slope) - mean(changes_in_em_slope)) / pooled_std_dev
 print(f"effect size: {cohens_d}")
 print()